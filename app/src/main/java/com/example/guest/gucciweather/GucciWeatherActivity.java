f41f17241e407196aec634bee0bc2da5325e36d6
andrew telleria and david remington <andrew@>
2016-04-25 15:23:13 -0700

    initial commit

---
+++ app/src/main/java/com/example/guest/gucciweather/GucciWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.gucciweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GucciWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gucci_weather);
+    }
+}
